28e2694fdf4603200680d24736d66bc55a716d9e
SrPattif <gustavo.rodrigues@grupomadero.com.br>
2024-01-29 10:25:30 -0300

    campos do objeto Notification alterados para anuável/required

--- Gustavo.NotificationTestAPI/Model/Notification.cs
+++ Gustavo.NotificationTestAPI/Model/Notification.cs
@@ -6,12 +6,12 @@ namespace Gustavo.NotificationTestAPI.Model
     {
 
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string InteractionURL { get; set; }
-        public string ImageURL { get; set; }
-        public string Type { get; set; }
-        public string DisplayType { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public string? InteractionURL { get; set; }
+        public string? ImageURL { get; set; }
+        public string? Type { get; set; }
+        public string? DisplayType { get; set; }
         public int? UserId { get; set; }
         public DateTime CreatedAt { get; set; }
         public bool IsPublic { get; set; }
